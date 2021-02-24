23850350cfb769170d6df5afb39cefde488db0f1
LINETH-03 <lperezh9@miumg.edu.gt>
2021-02-24 01:36:15 -0600

    all folders

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+	// write your code here
+        System.out.println("Hola Mundoo!");
+        System.out.println("Lily Lineth");
+    }
+}
